355297100e28b16acae01a3da13b2494e5952943
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-04-20 21:49:26 +0800

    adding the algorithm for the Goal Parser Interpretation problem.

---
+++ src/main/java/com/practice/string/GoalParserInterpretation.java
@@ -0,0 +1,24 @@
+package com.practice.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GoalParserInterpretation {
+
+  public String interpret(String command) {
+    Map<String, String> map = new HashMap<>();
+    map.put("G", "G");
+    map.put("()", "o");
+    map.put("(al)", "al");
+    StringBuilder sb = new StringBuilder();
+    StringBuffer temp = new StringBuffer();
+    for (int i = 0; i < command.length(); i++) {
+      temp.append(command.charAt(i));
+      if (map.containsKey(temp.toString())) {
+        sb.append(map.get(temp.toString()));
+        temp = new StringBuffer();
+      }
+    }
+    return sb.toString();
+  }
+}
