5a5fcb1b5cd9dd3db58d298bbe80f5f80f672f71
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-04-30 21:00:02 +0800

    Adding more tests for the merging intervals.

--- src/test/java/com/practice/array/MergeIntervalsTest.java
+++ src/test/java/com/practice/array/MergeIntervalsTest.java
@@ -17,4 +17,26 @@ public class MergeIntervalsTest {
     //THEN
     MatcherAssert.assertThat(mergedInterval, Is.is(new int[][]{{1, 6}, {8, 10}, {15, 18}}));
   }
+
+  @DisplayName("GIVEN an array with only 1 interval WHEN merge is called THEN it returns the given array")
+  @Test
+  public void testMergeWith1Intervals() {
+    //GIVEN
+    int[][] intervals = {{1, 3}};
+    //WHEN
+    int[][] mergedInterval = new MergeIntervals().merge(intervals);
+    //THEN
+    MatcherAssert.assertThat(mergedInterval, Is.is(new int[][]{{1, 3}}));
+  }
+
+  @DisplayName("GIVEN an array with only 2 intervals one contained another one within it WHEN merge is called THEN it returns the array of merged intverval")
+  @Test
+  public void testMergeWith2Intervals1Contains2ndWithinIt() {
+    //GIVEN
+    int[][] intervals = {{1, 10}, {2, 5}};
+    //WHEN
+    int[][] mergedInterval = new MergeIntervals().merge(intervals);
+    //THEN
+    MatcherAssert.assertThat(mergedInterval, Is.is(new int[][]{{1, 10}}));
+  }
 }
