b179f6c7cae985792438e3f040e0bb761bc0cfc0
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-05-09 21:17:02 +0800

    Algorithm to find the top K frequent elements.

---
+++ src/main/java/com/practice/array/TopKFrequentElements.java
@@ -0,0 +1,31 @@
+package com.practice.array;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class TopKFrequentElements {
+
+  public int[] topKFrequent(int[] nums, int k) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for(int i=0; i < nums.length;  i++){
+      map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+    }
+
+    PriorityQueue<int[]> queue = new PriorityQueue<>((v1, v2) -> v1[1] - v2[1]);
+    for(int i:map.keySet()) {
+      queue.add(new int[]{i, map.get(i)});
+      if(queue.size() > k){
+        queue.poll();
+      }
+    }
+
+    int[] result = new int[k];
+    int temp = 0;
+    while(temp < k) {
+      int[] val = queue.poll();
+      result[temp++] = val[0];
+    }
+    return result;
+  }
+}
