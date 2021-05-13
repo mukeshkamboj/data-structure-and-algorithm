914bc06e112659868a2287dcfb2d47b7fb9d1a26
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-05-13 19:56:16 +0800

    Adding test for the smallest sub array logic

--- src/test/java/com/practice/sliding_window/SmallestSubArrayGreaterThanEqualToGivenSumTest.java
+++ src/test/java/com/practice/sliding_window/SmallestSubArrayGreaterThanEqualToGivenSumTest.java
@@ -1,44 +1,30 @@
 package com.practice.sliding_window;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class SmallestSubArrayGreaterThanEqualToGivenSumTest {
 
-  @Test
-  public void test_1() {
-    int smallestSubArrayLength = SmallestSubArrayGreaterThanEqualToGivenSum
-        .findSmallestSubArrayLength(100, new int[]{7, 2, 1, 5, 2, 3, 2});
-    assertThat(0, is(smallestSubArrayLength));
+  @ParameterizedTest
+  @MethodSource("testDataProvider")
+  public void testFindSmallestSubArrayLength(int givenSum, int[] arr, int expected) {
+    //WHEN
+    int smallestSubArrayLength = new SmallestSubArrayGreaterThanEqualToGivenSum()
+        .minSubArrayLen(givenSum, arr);
+    //THEN
+    Assertions.assertEquals(expected, smallestSubArrayLength);
   }
 
-  @Test
-  public void test_2() {
-    int smallestSubArrayLength = SmallestSubArrayGreaterThanEqualToGivenSum
-        .findSmallestSubArrayLength(7, new int[]{7, 2, 1, 5, 2, 3, 2});
-    assertThat(1, is(smallestSubArrayLength));
-  }
-
-  @Test
-  public void test_3() {
-    int smallestSubArrayLength = SmallestSubArrayGreaterThanEqualToGivenSum
-        .findSmallestSubArrayLength(7, new int[]{2, 1, 5, 2, 3, 2});
-    assertThat(2, is(smallestSubArrayLength));
-  }
-
-  @Test
-  public void test_4() {
-    int smallestSubArrayLength = SmallestSubArrayGreaterThanEqualToGivenSum
-        .findSmallestSubArrayLength(7, new int[]{2, 1, 5, 2, 8});
-    assertThat(1, is(smallestSubArrayLength));
-  }
-
-  @Test
-  public void test_5() {
-    int smallestSubArrayLength = SmallestSubArrayGreaterThanEqualToGivenSum
-        .findSmallestSubArrayLength(8, new int[]{3, 4, 1, 1, 6});
-    assertThat(3, is(smallestSubArrayLength));
+  static Stream<Arguments> testDataProvider() {
+    return Stream.of(
+        Arguments.arguments(100, new int[]{7, 2, 1, 5, 2, 3, 2}, 0),
+        Arguments.arguments(7, new int[]{2, 1, 5, 2, 3, 2}, 2),
+        Arguments.arguments(7, new int[]{2, 1, 5, 2, 8}, 1),
+        Arguments.arguments(8, new int[]{3, 4, 1, 1, 6}, 3),
+        Arguments.arguments(7, new int[]{7, 2, 1, 5, 2, 3, 2}, 1)
+    );
   }
 }
