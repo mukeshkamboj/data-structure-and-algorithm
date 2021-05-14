0b7fb619c3c4ab0d7fd029cacc7777ecf299bef5
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-05-14 21:39:50 +0800

    Adding the edge case scenarios.

--- src/test/java/com/practice/sliding_window/MaxSumOfContiguousSubArrayTest.java
+++ src/test/java/com/practice/sliding_window/MaxSumOfContiguousSubArrayTest.java
@@ -1,21 +1,29 @@
 package com.practice.sliding_window;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class MaxSumOfContiguousSubArrayTest {
 
-  @Test
-  public void test_1() {
-    int maxSumOfSubArray = MaxSumOfContiguousSubArray.findMaxSum(3, new int[]{2, 1, 5, 1, 3, 2});
-    assertThat(9, is(maxSumOfSubArray));
+  @ParameterizedTest
+  @MethodSource("testDataProvider")
+  public void testFindMaxSum(int subArraySize, int[] arr, int expected) {
+    //WHEN
+    var actualSum = MaxSumOfContiguousSubArray.findMaxSum(subArraySize, arr);
+    //THEN
+    Assertions.assertEquals(expected, actualSum);
   }
 
-  @Test
-  public void test_2() {
-    int maxSumOfSubArray = MaxSumOfContiguousSubArray.findMaxSum(2, new int[]{2, 3, 4, 1, 5});
-    assertThat(7, is(maxSumOfSubArray));
+  static Stream<Arguments> testDataProvider() {
+    return Stream.of(
+        Arguments.arguments(3, new int[]{2, 1, 5, 1, 3, 2}, 9),
+        Arguments.arguments(6, new int[]{2, 1, 5, 1, 3, 2}, 14),
+        Arguments.arguments(1, new int[]{2}, 2),
+        Arguments.arguments(0, new int[]{}, 0),
+        Arguments.arguments(2, new int[]{2, 3, 4, 1, 5}, 7)
+    );
   }
 }
