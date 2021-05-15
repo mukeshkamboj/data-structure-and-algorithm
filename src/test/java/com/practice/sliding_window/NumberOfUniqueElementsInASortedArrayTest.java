7e6843d7488397af6484d158a08502abd0f32ab1
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-05-15 14:36:03 +0800

    Refactoring of code

--- src/test/java/com/practice/sliding_window/NumberOfUniqueElementsInASortedArrayTest.java
+++ src/test/java/com/practice/sliding_window/NumberOfUniqueElementsInASortedArrayTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.core.Is.is;
 
-public class NumberOfUniqueElementsInAnArrayTest {
+public class NumberOfUniqueElementsInASortedArrayTest {
 
   @Test
   public void test_1() {
-    int uniqueElements = NumberOfUniqueElementsInAnArray.find(new int[]{2, 3, 3, 3, 6, 9, 9});
+    int uniqueElements = NumberOfUniqueElementsInASortedArray.find(new int[]{2, 3, 3, 3, 6, 9, 9});
     MatcherAssert.assertThat(uniqueElements, is(4));
   }
 
   @Test
   public void test_2() {
-    int uniqueElements = NumberOfUniqueElementsInAnArray.find(new int[]{2, 2, 2, 11});
+    int uniqueElements = NumberOfUniqueElementsInASortedArray.find(new int[]{2, 2, 2, 11});
     MatcherAssert.assertThat(uniqueElements, is(2));
   }
 }
