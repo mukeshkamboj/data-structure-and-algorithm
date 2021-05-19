f1f386a2616a1295bc92a968b7b70432736b5ccb
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-05-19 20:23:14 +0800

    Improving the moving zeros algorithm.

--- src/main/java/com/practice/array/MoveZeroes.java
+++ src/main/java/com/practice/array/MoveZeroes.java
@@ -3,14 +3,17 @@ package com.practice.array;
 public class MoveZeroes {
 
   public void moveZeroes(int[] arr) {
-    int position = 0;
-    for (int value : arr) {
-      if (value != 0) {
-        arr[position++] = value;
+    int zp = 0;
+    int nzp = 0;
+    while (nzp < arr.length) {
+      if (arr[nzp] != 0) {
+        arr[zp] = arr[nzp];
+        if (zp != nzp) {
+          arr[nzp] = 0;
+        }
+        zp++;
       }
-    }
-    for (int i = position; i < arr.length; i++) {
-      arr[i] = 0;
+      nzp++;
     }
   }
 }
