8bceb21fb4b11f23ca25dda58bf868e76d85d5a2
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-05-22 21:14:24 +0800

    Refactoring the test cases for LIS

--- src/test/java/com/practice/dynamic_programming/LongestIncreasingSubSequence_O_N_SquareTest.java
+++ src/test/java/com/practice/dynamic_programming/LongestIncreasingSubSequence_O_N_SquareTest.java
@@ -1,64 +1,29 @@
 package com.practice.dynamic_programming;
 
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.core.Is;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class LongestIncreasingSubSequence_O_N_SquareTest {
 
-  @DisplayName("GIVEN an array with no increase sequence WHEN findMax is invoked THEN it returns 1")
-  @Test
-  public void testFindMaxWithDecreasingSequences() {
+  @ParameterizedTest
+  @MethodSource("testDataProvider")
+  public void testFindMax(int[] arr, int expectedMax) {
     //WHEN
-    int[] arr = {10, 9, 8, 7, 6, 5, 4, 3};
-    //GIVEN
-    int max = new LongestIncreasingSubSequence_O_N_Square().findMax(arr);
+    var max = new LongestIncreasingSubSequence_O_N_Square().findMax(arr);
     //THEN
-    MatcherAssert.assertThat(max, Is.is(1));
+    Assertions.assertEquals(expectedMax, max);
   }
 
-  @DisplayName("GIVEN an array with the multiple increase sequence WHEN findMax is invoked THEN it returns the size of longest LIS")
-  @Test
-  public void testFindMaxWithMultipleIncreasingSequences() {
-    //WHEN
-    int[] arr = {10, 9, 2, 5, 3, 7, 101, 18};
-    //GIVEN
-    int max = new LongestIncreasingSubSequence_O_N_Square().findMax(arr);
-    //THEN
-    MatcherAssert.assertThat(max, Is.is(4));
-  }
-
-  @DisplayName("GIVEN an array with elements sorted in the increasing order WHEN findMax is invoked THEN it returns the size of the array")
-  @Test
-  public void testFindMaxWithSortedArrayInIncreasingOrder() {
-    //WHEN
-    int[] arr = {10, 11, 12, 13, 14, 15};
-    //GIVEN
-    int max = new LongestIncreasingSubSequence_O_N_Square().findMax(arr);
-    //THEN
-    MatcherAssert.assertThat(max, Is.is(arr.length));
-  }
-
-  @DisplayName("GIVEN an array with single element WHEN findMax is invoked THEN it returns 1")
-  @Test
-  public void testFindMaxWithArrayWithSingleElement() {
-    //WHEN
-    int[] arr = {10};
-    //GIVEN
-    int max = new LongestIncreasingSubSequence_O_N_Square().findMax(arr);
-    //THEN
-    MatcherAssert.assertThat(max, Is.is(1));
-  }
-
-  @DisplayName("GIVEN an empty array WHEN findMax is invoked THEN it returns 0")
-  @Test
-  public void testFindMaxWithEmptyArray() {
-    //WHEN
-    int[] arr = {};
-    //GIVEN
-    int max = new LongestIncreasingSubSequence_O_N_Square().findMax(arr);
-    //THEN
-    MatcherAssert.assertThat(max, Is.is(0));
+  static Stream<Arguments> testDataProvider() {
+    return Stream.of(
+        Arguments.arguments(new int[]{10, 9, 8, 7, 6, 5, 4, 3}, 1),
+        Arguments.arguments(new int[]{10, 11, 12, 13, 14, 15}, 6),
+        Arguments.arguments(new int[]{10}, 1),
+        Arguments.arguments(new int[]{}, 0),
+        Arguments.arguments(new int[]{10, 9, 2, 5, 3, 7, 101, 18}, 4)
+    );
   }
 }
