a3627a34286ddf704af12e88a28ee129fc9f09b7
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-06-02 09:24:35 +0800

    Algorithm to find the max profit with tabulation dp

--- src/main/java/com/practice/dynamic_programming/knapsack/zero_one/MaxProfitAfterKeepingItemsInGivenBag.java
+++ src/main/java/com/practice/dynamic_programming/knapsack/zero_one/MaxProfitAfterKeepingItemsInGivenBag.java
@@ -3,7 +3,15 @@ package com.practice.dynamic_programming.knapsack.zero_one;
 // https://www.geeksforgeeks.org/knapsack-with-large-weights/
 public class MaxProfitAfterKeepingItemsInGivenBag {
 
+  static int[][] dp;
+
   public int findMaxProfit(int[] weight, int[] price, int knapsackWeight) {
+    dp = new int[weight.length + 1][knapsackWeight + 1];
+    for (int i = 0; i <= weight.length; i++) {
+      for (int j = 0; j <= knapsackWeight; j++) {
+        dp[i][j] = -1;
+      }
+    }
     return knapsack(weight, price, knapsackWeight, 0);
   }
 
@@ -12,12 +20,17 @@ public class MaxProfitAfterKeepingItemsInGivenBag {
       return 0;
     }
 
+    if (dp[index][knapsackWeight] != -1) {
+      return dp[index][knapsackWeight];
+    }
     if (weight[index] <= knapsackWeight) {
-      return Math.max(
+      dp[index][knapsackWeight] = Math.max(
           price[index] + knapsack(weight, price, knapsackWeight - weight[index], index + 1),
           knapsack(weight, price, knapsackWeight, index + 1));
     } else {
-      return knapsack(weight, price, knapsackWeight, index + 1);
+      dp[index][knapsackWeight] = knapsack(weight, price, knapsackWeight, index + 1);
     }
+
+    return dp[index][knapsackWeight];
   }
 }
