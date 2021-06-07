02c5dd6ccc69934d6244efa57aec5bc735239ccb
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-06-07 22:46:17 +0800

    Updating the tests.

--- src/test/java/com/practice/array/ProductOfArrayExceptSelfTest.java
+++ src/test/java/com/practice/array/ProductOfArrayExceptSelfTest.java
@@ -1,64 +1,30 @@
 package com.practice.array;
 
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.core.Is;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class ProductOfArrayExceptSelfTest {
 
-  @DisplayName("GIVEN an array with natural numbers WHEN productExceptSelf is invoked THEN it returns an array with products except itself")
-  @Test
-  public void testProductExceptSelfWithNaturalNumber() {
-    //GIVEN
-    int[] arr = {1, 2, 3, 4, 5};
+  @ParameterizedTest
+  @MethodSource("testDataSource")
+  public void testProductExceptSelf(int[] arr, int[] expected) {
     //WHEN
-    int[] result = new ProductOfArrayExceptSelf().productExceptSelf(arr);
+    var actual = new ProductOfArrayExceptSelf().productExceptSelf(arr);
     //THEN
-    MatcherAssert.assertThat(result, Is.is(new int[]{120, 60, 40, 30, 24}));
+    Assertions.assertArrayEquals(expected, actual);
   }
 
-  @DisplayName("GIVEN an array with non zeroes and one negative numbers WHEN productExceptSelf is invoked THEN it returns an array with products except itself")
-  @Test
-  public void testProductExceptSelfWithArrayContainingNonZeroNegativeNumbers() {
-    //GIVEN
-    int[] arr = {1, 2, -3, 4, 5};
-    //WHEN
-    int[] result = new ProductOfArrayExceptSelf().productExceptSelf(arr);
-    //THEN
-    MatcherAssert.assertThat(result, Is.is(new int[]{-120, -60, 40, -30, -24}));
-  }
-
-  @DisplayName("GIVEN an array containing one zero WHEN productExceptSelf is invoked THEN it returns an array with products except itself")
-  @Test
-  public void testProductExceptSelfWithArrayContainingZero() {
-    //GIVEN
-    int[] arr = {1, 2, 0, 4, 5};
-    //WHEN
-    int[] result = new ProductOfArrayExceptSelf().productExceptSelf(arr);
-    //THEN
-    MatcherAssert.assertThat(result, Is.is(new int[]{0, 0, 40, 0, 0}));
-  }
-
-  @DisplayName("GIVEN an array containing more than one zeroes WHEN productExceptSelf is invoked THEN it returns an array with products except itself")
-  @Test
-  public void testProductExceptSelfWithArrayContainingMoreThanOneZeroes() {
-    //GIVEN
-    int[] arr = {1, 2, 0, 4, 5, 0};
-    //WHEN
-    int[] result = new ProductOfArrayExceptSelf().productExceptSelf(arr);
-    //THEN
-    MatcherAssert.assertThat(result, Is.is(new int[]{0, 0, 0, 0, 0, 0}));
-  }
-
-  @DisplayName("GIVEN an empty array WHEN productExceptSelf is invoked THEN it returns an empty array")
-  @Test
-  public void testProductExceptSelfWithEmptyArray() {
-    //GIVEN
-    int[] arr = {};
-    //WHEN
-    int[] result = new ProductOfArrayExceptSelf().productExceptSelf(arr);
-    //THEN
-    MatcherAssert.assertThat(result, Is.is(new int[]{}));
+  static Stream<Arguments> testDataSource() {
+    return Stream.of(
+        Arguments.arguments(new int[]{1, 2, 3, 4, 5}, new int[]{120, 60, 40, 30, 24}),
+        Arguments.arguments(new int[]{1, 2, -3, 4, 5}, new int[]{-120, -60, 40, -30, -24}),
+        Arguments.arguments(new int[]{1, 2, 0, 4, 5}, new int[]{0, 0, 40, 0, 0}),
+        Arguments.arguments(new int[]{1, 2, 0, 4, 5, 0}, new int[]{0, 0, 0, 0, 0, 0}),
+        Arguments.arguments(new int[]{1, 2, 0, 4, 5, 0}, new int[]{0, 0, 0, 0, 0, 0}),
+        Arguments.arguments(new int[]{}, new int[]{})
+    );
   }
 }
