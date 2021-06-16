14dd2e9ca99911c4ad6e973b4b31ed88b6878522
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-06-16 21:34:36 +0800

    Algorithm to find the n times repeated number.

---
+++ src/main/java/com/practice/array/NRepeatedElementInSize2NArray.java
@@ -0,0 +1,14 @@
+package com.practice.array;
+
+//https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+public class NRepeatedElementInSize2NArray {
+
+  public int repeatedNTimes(int[] A) {
+    for(int i=2;i<A.length;i++){
+      if(A[i] == A[i-1] || A[i] == A[i-2]){
+        return A[i];
+      }
+    }
+    return A[0];
+  }
+}
