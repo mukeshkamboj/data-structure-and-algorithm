4725b74984bbc9381380a6ed317adda20dced81b
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-06-17 23:38:51 +0800

    Updating the tests of the maximum ascending subarray sum algorithm

--- src/test/java/com/practice/array/MaximumAscendingSubArraySumTest.java
+++ src/test/java/com/practice/array/MaximumAscendingSubArraySumTest.java
@@ -1,86 +1,31 @@
 package com.practice.array;
 
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.core.Is;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class MaximumAscendingSubArraySumTest {
 
-  @DisplayName("GIVEN an array with multiple subArrays containing elements in ascending order WHEN maxAscendingSum is invoked THEN it returns the max sum of elements of the subArray")
-  @Test
-  public void testMaxAscendingSumMultipleSubArrayWithElementsInAscendingOrder() {
-    //GIVEN
-    int[] arr = {12, 17, 15, 13, 10, 11, 12, 5, 6, 7};
+  @ParameterizedTest
+  @MethodSource("testDataProvider")
+  public void testMaxAscendingSum(int[] arr, int expected) {
     //WHEN
-    int sum = new MaximumAscendingSubArraySum().maxAscendingSum(arr);
+    var sum = new MaximumAscendingSubArraySum().maxAscendingSum(arr);
     //THEN
-    MatcherAssert.assertThat(sum, Is.is(33));
+    Assertions.assertEquals(expected, sum);
   }
 
-  @DisplayName("GIVEN an array with multiple subArrays containing elements in ascending order and there is sub array array with single element which larger then the sum of all other sub array WHEN maxAscendingSum is invoked THEN it returns the subArray with single element")
-  @Test
-  public void testMaxAscendingSumArrayWithAnElementGreaterThenSumOfSubArrays() {
-    //GIVEN
-    int[] arr = {100, 12, 17, 15, 13, 10, 11, 12, 5, 6, 7};
-    //WHEN
-    int sum = new MaximumAscendingSubArraySum().maxAscendingSum(arr);
-    //THEN
-    MatcherAssert.assertThat(sum, Is.is(100));
-  }
-
-  @DisplayName("GIVEN an array with all elements in ascending order WHEN maxAscendingSum is invoked THEN it returns the sum of all elements.")
-  @Test
-  public void testMaxAscendingSumArrayElementsAreInAscendingOrder() {
-    //GIVEN
-    int[] arr = {10, 11, 12};
-    //WHEN
-    int sum = new MaximumAscendingSubArraySum().maxAscendingSum(arr);
-    //THEN
-    MatcherAssert.assertThat(sum, Is.is(33));
-  }
-
-  @DisplayName("GIVEN an array with all elements in descending order WHEN maxAscendingSum is invoked THEN it returns the first element of the array.")
-  @Test
-  public void testMaxAscendingSumArrayHasAllElementInDescendingOrder() {
-    //GIVEN
-    int[] arr = {50, 34, 21, 12};
-    //WHEN
-    int sum = new MaximumAscendingSubArraySum().maxAscendingSum(arr);
-    //THEN
-    MatcherAssert.assertThat(sum, Is.is(50));
-  }
-
-  @DisplayName("GIVEN an array with one number's duplicate WHEN maxAscendingSum is invoked THEN it returns the first element of the array.")
-  @Test
-  public void testMaxAscendingSumArrayHasDuplicatesOfSingleNumber() {
-    //GIVEN
-    int[] arr = {10, 10, 10, 10, 10};
-    //WHEN
-    int sum = new MaximumAscendingSubArraySum().maxAscendingSum(arr);
-    //THEN
-    MatcherAssert.assertThat(sum, Is.is(10));
-  }
-
-  @DisplayName("GIVEN an array with one number WHEN maxAscendingSum is invoked THEN it returns the first element of the array.")
-  @Test
-  public void testMaxAscendingSumArrayHasSingleElement() {
-    //GIVEN
-    int[] arr = {10};
-    //WHEN
-    int sum = new MaximumAscendingSubArraySum().maxAscendingSum(arr);
-    //THEN
-    MatcherAssert.assertThat(sum, Is.is(10));
-  }
-
-  @DisplayName("GIVEN an empty array WHEN maxAscendingSum is invoked THEN it returns 0")
-  @Test
-  public void testMaxAscendingSumArrayIsEmpty() {
-    //GIVEN
-    int[] arr = {};
-    //WHEN
-    int sum = new MaximumAscendingSubArraySum().maxAscendingSum(arr);
-    //THEN
-    MatcherAssert.assertThat(sum, Is.is(0));
+  static Stream<Arguments> testDataProvider() {
+    return Stream.of(
+        Arguments.arguments(new int[]{12, 17, 15, 13, 10, 11, 12, 5, 6, 7}, 33),
+        Arguments.arguments(new int[]{100, 12, 17, 15, 13, 10, 11, 12, 5, 6, 7}, 100),
+        Arguments.arguments(new int[]{10, 11, 12}, 33),
+        Arguments.arguments(new int[]{50, 34, 21, 12}, 50),
+        Arguments.arguments(new int[]{10, 10, 10, 10, 10}, 10),
+        Arguments.arguments(new int[]{10}, 10),
+        Arguments.arguments(new int[]{}, 0)
+    );
   }
 }
