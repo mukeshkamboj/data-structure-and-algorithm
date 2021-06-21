c86e99e0b6482ec6275c233aeb7c64d80ac48f3a
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-06-21 22:24:35 +0800

    Algorithm to find the range sum of bst

---
+++ src/main/java/com/practice/tree/RangeSumOfBST.java
@@ -0,0 +1,42 @@
+package com.practice.tree;
+
+//https://leetcode.com/problems/range-sum-of-bst/
+public class RangeSumOfBST {
+
+  public int rangeSumBST(TreeNode root, int low, int high) {
+
+    if (root == null) {
+      return 0;
+    }
+
+    if (root.val < low) {
+      return rangeSumBST(root.right, low, high);
+    }
+
+    if (root.val > high) {
+      return rangeSumBST(root.left, low, high);
+    }
+
+    return root.val + rangeSumBST(root.right, low, high) + rangeSumBST(root.left, low, high);
+  }
+
+  private static class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
