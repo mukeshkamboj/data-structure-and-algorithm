b973d717cb04cea341898220bb7fd5949c929523
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-06-22 22:39:30 +0800

    Test for the algorithm to find the range sum in BST

--- src/test/java/com/practice/tree/RangeSumOfBSTTest.java
+++ src/test/java/com/practice/tree/RangeSumOfBSTTest.java
@@ -6,21 +6,21 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class MaximumDepthOfBinaryTreeTest {
+public class RangeSumOfBSTTest {
 
   @ParameterizedTest
   @MethodSource("testDataProvider")
-  public void findMaxDepth(TreeNode root, int expected) {
+  public void findRangeSum(TreeNode root, int low, int high, int expected) {
     //WHEN
-    var val = new MaximumDepthOfBinaryTree().maxDepth(root);
+    var val = new RangeSumOfBST().rangeSumBST(root, low, high);
     //THEN
     Assertions.assertEquals(expected, val);
   }
 
   static Stream<Arguments> testDataProvider() {
     return Stream.of(
-        Arguments.arguments(TreeUtil.build(new int[]{5, 4, 3, 2, 1}), 5),
-        Arguments.arguments(TreeUtil.build(new int[]{5, 4, 6, 2, 8}), 3)
+        Arguments.arguments(TreeUtil.build(new int[]{10, 5, 15, 3, 7, 18}), 7, 15, 32),
+        Arguments.arguments(TreeUtil.build(new int[]{10, 5, 15, 3, 7, 13, 18, 1, 6}), 6, 10, 23)
     );
   }
 }
