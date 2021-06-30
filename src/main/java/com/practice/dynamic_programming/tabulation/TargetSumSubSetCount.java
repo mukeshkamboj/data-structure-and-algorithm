374a8d772da94850a6cf0e4640f70d0dd0ba2ff1
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-06-30 19:38:14 +0800

    Algorithm to find the target sum sub set count

---
+++ src/main/java/com/practice/dynamic_programming/tabulation/TargetSumSubSetCount.java
@@ -0,0 +1,8 @@
+package com.practice.dynamic_programming.tabulation;
+
+public class TargetSumSubSetCount {
+
+  public int count(int[] arr, int sum) {
+    return 0;
+  }
+}
---
+++ src/test/java/com/practice/dynamic_programming/tabulation/TargetSumSubSetCountTest.java
@@ -0,0 +1,31 @@
+package com.practice.dynamic_programming.tabulation;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class TargetSumSubSetCountTest {
+
+  @ParameterizedTest
+  @MethodSource("testDataProvider")
+  public void testCount(int[] arr, int sum, int expected) {
+    //WHEN
+    var exists = new TargetSumSubSetCount().count(arr, sum);
+    //THEN
+    Assertions.assertEquals(expected, exists);
+  }
+
+  static Stream<Arguments> testDataProvider() {
+    return Stream.of(
+        Arguments.arguments(new int[]{3, 4, 5, 6}, 9, 2),
+        Arguments.arguments(new int[]{3, 4, 5, 6}, 15, 1),
+        Arguments.arguments(new int[]{3}, 3, 1),
+        Arguments.arguments(new int[]{3, 1}, 4, 1),
+        Arguments.arguments(new int[]{1, 1, 1, 1, 1}, 4, 2),
+        Arguments.arguments(new int[]{3, 4, 5, 6}, 100, 0)
+    );
+  }
+
+}
