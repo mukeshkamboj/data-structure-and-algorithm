f895c7b78b4be8c41a52a6a85a3f6dd73e5680f1
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-07-11 11:06:53 +0800

    Recursive algorithm to merge two lists.

---
+++ src/main/java/com/practice/recursion/linkedList/MergeTwoSortedLists.java
@@ -0,0 +1,24 @@
+package com.practice.recursion.linkedList;
+
+import com.practice.linked_list.ListNode;
+
+public class MergeTwoSortedLists {
+
+  public ListNode mergeSortedList(ListNode l1, ListNode l2) {
+    if (l1 == null) {
+      return l2;
+    }
+
+    if (l2 == null) {
+      return l1;
+    }
+
+    if (l1.getVal() <= l2.getVal()) {
+      l1.setNext(mergeSortedList(l1.getNext(), l2));
+      return l1;
+    } else {
+      l2.setNext(mergeSortedList(l1, l2.getNext()));
+      return l2;
+    }
+  }
+}
