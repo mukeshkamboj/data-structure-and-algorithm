2dae195ce27929ca4a9aa33358780365b10ff690
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-07-14 18:00:03 +0800

    Algorithm to generate the parenthesis.

---
+++ src/main/java/com/practice/recursion/string/GenerateParentheses.java
@@ -0,0 +1,27 @@
+package com.practice.recursion.string;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GenerateParentheses {
+
+  private List<String> parenthesis = new LinkedList<>();
+
+  public List<String> generateParenthesis(int n) {
+    generate("", 0, 0, n);
+    return parenthesis;
+  }
+
+  private void generate(String str, int open, int close, int max) {
+    if (str.length() == 2 * max) {
+      parenthesis.add(str);
+      return;
+    }
+    if (open < max) {
+      generate(str + "(", open + 1, close, max);
+    }
+    if (close < open) {
+      generate(str + ")", open, close + 1, max);
+    }
+  }
+}
