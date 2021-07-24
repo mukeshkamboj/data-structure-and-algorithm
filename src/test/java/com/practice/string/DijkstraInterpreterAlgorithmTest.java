9ddec62f95532132f42d80c0df7b7253d31a3e98
Mukesh Kumar <mukesh.shyamlal@gmail.com>
2021-07-24 22:39:50 +0800

    Used the junit 5.0 for unit test.

--- src/test/java/com/practice/string/DijkstraInterpreterAlgorithmTest.java
+++ src/test/java/com/practice/string/DijkstraInterpreterAlgorithmTest.java
@@ -1,25 +1,28 @@
 package com.practice.string;
 
-import org.junit.jupiter.api.Test;
+import static java.lang.Double.POSITIVE_INFINITY;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class DijkstraInterpreterAlgorithmTest {
 
-  @Test
-  public void GIVE_correct_expression_WHEN_interpret_function_is_called_THEN_value_is_returned() {
-    DijkstraInterpreterAlgorithm dijkstraInterpreterAlgorithm = new DijkstraInterpreterAlgorithm();
-    String expression = "( 100 / ( ( ( 2 + 1 ) - 1 ) * 5 ) )";
-    double result = dijkstraInterpreterAlgorithm.interpret(expression);
-    assertThat(10.0, is(result));
+  @ParameterizedTest
+  @MethodSource("testDataProvider")
+  public void testInterpret(String expression, double expected) {
+    //WHEN
+    var result = new DijkstraInterpreterAlgorithm().interpret(expression);
+    //THEN
+    Assertions.assertEquals(expected, result);
   }
 
-  @Test
-  public void GIVE_expression_which_evaluated_to_infinity_divided_by_zero_condition_WHEN_interpret_function_is_called_THEN_double_with_infinity_is_returned() {
-    DijkstraInterpreterAlgorithm dijkstraInterpreterAlgorithm = new DijkstraInterpreterAlgorithm();
-    String expression = "( 100 / ( ( ( 2 + 1 ) - 3 ) * 5 ) )";
-    Double result = dijkstraInterpreterAlgorithm.interpret(expression);
-    assertThat(result.isInfinite(), is(true));
+  static Stream<Arguments> testDataProvider() {
+    return Stream.of(
+        Arguments.arguments("( 100 / ( ( ( 2 + 1 ) - 1 ) * 5 ) )", 10.0),
+        Arguments.arguments("( 100 / ( ( ( 2 + 1 ) - 3 ) * 5 ) )", POSITIVE_INFINITY)
+    );
   }
 }
